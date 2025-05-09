7d43310980dbecdd50ba2a242c250731d41b220f
Brayan Alpizar <brayanalpizar36@gmail.com>
2025-05-08 22:47:05 -0600

    Conexion con el SQL terminada con una base de ejemplo, ademas se agregaron todos los modelos en una version preeliminar

---
+++ CEDigital/Models/Admin_model.cs
@@ -0,0 +1,10 @@
+﻿namespace CEDigital.Models
+{
+    //Clase que representa un administrador
+    public class Admin_model
+    {
+        public int admin_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ CEDigital/Models/Student_model.cs
@@ -0,0 +1,18 @@
+﻿namespace CEDigital.Models
+{
+    //Clase que representa a un estudiante
+    public class Student_model
+    {
+        public string student_id { get; set; }
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string id_number { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+
+        // Relación con grupos
+        public List<int> group_ids { get; set; }
+    }
+}
