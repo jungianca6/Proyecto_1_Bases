7cef3d1bfaae937e85a140419cdf08735f047c7e
Brayan Alpizar <brayanalpizar36@gmail.com>
2025-05-08 22:47:05 -0600

    Conexion con el SQL terminada con una base de ejemplo, ademas se agregaron todos los modelos en una version preeliminar

---
+++ CEDigital/Models/News_model.cs
@@ -0,0 +1,14 @@
+﻿namespace CEDigital.Models
+{
+    //Clase que representa una noticia
+    public class News_model
+    {
+        public int news_id { get; set; }
+        public string message { get; set; }
+        public string title { get; set; }
+
+        //FK de profesor
+        public string professor_id { get; set; }
+    }
+
+}
---
+++ CEDigital/Models/Semester_model.cs
@@ -0,0 +1,9 @@
+﻿namespace CEDigital.Models
+{
+    public class Semester_model
+    {
+        public int semester_id { get; set; }
+        public int year { get; set; }
+    }
+
+}
