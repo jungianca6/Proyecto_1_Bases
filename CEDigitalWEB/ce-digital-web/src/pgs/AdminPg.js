e8dc82f6c45b6573eae4b90c2f83dab9407759ae
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-05-10 16:43:51 -0600

    inicio
    investigar sobre bootstrap

--- CEDigitalWEB/ce-digital-web/src/pgs/AdminPg.js
+++ CEDigitalWEB/ce-digital-web/src/pgs/AdminPg.js
@@ -1,3 +1,10 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import React from "react";
+
+
 function AdminPg() {
     return <div>Página del Administrador</div>;
   }
