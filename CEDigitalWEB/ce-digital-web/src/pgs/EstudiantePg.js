5d1861f88e9b52a10cae6f6ded185d14b140b0a2
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-05-11 16:09:16 -0600

    boostrap y CSS para botones
    que bendicion

--- CEDigitalWEB/ce-digital-web/src/pgs/EstudiantePg.js
+++ CEDigitalWEB/ce-digital-web/src/pgs/EstudiantePg.js
@@ -3,20 +3,32 @@ import { useNavigate } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 import React from "react";
-import Button from 'react-bootstrap/Button';
-
+import { Button } from 'react-bootstrap';
+import styles from './EstudiantePg.module.css';
 
 function EstudiantePg() {
+    const handleButton1Click = () => {
+        alert("Botón en (255, 100) clickeado");
+    };
+
+    const handleButton2Click = () => {
+        alert("Botón púrpura clickeado!");
+    };
+
     return (
-        <div
-            className="d-flex justify-content-center align-items-center"
-            style={{ height: "100vh" }}
-        >
-            <Button variant="primary">Prueba</Button>
-            <Button style={{ backgroundColor: "purple", borderColor: "purple" }}>
-                Prueba
+        <div className={styles.container}>
+            <Button
+                variant="primary"
+                className={styles.customButton1}
+                onClick={handleButton1Click}>
+                Botón 1
             </Button>
 
+            <Button
+                className={styles.customButton2}
+                onClick={handleButton2Click}>
+                Botón 2
+            </Button>
         </div>
     );
 }
