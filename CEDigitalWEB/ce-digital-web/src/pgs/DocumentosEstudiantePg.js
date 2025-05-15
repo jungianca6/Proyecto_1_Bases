947fd98aa3efde6762f8f3232cd80fed0ebb1fb1
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-05-15 01:27:09 -0600

    vista documentos
    hablar con christian si le pongo algo mas

--- CEDigitalWEB/ce-digital-web/src/pgs/DocumentosEstudiantePg.js
+++ CEDigitalWEB/ce-digital-web/src/pgs/DocumentosEstudiantePg.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { Button, Card, Form } from 'react-bootstrap';
+import styles from './DocumentosEstudiantePg.module.css'
+
 function DocumentosEstudiantePg() {
-    return <div>Documentos</div>;
+    return <div className={styles.documentosWrapper}>
+        <h1 className={styles.title}>CE Digital</h1>
+        <h3 className={styles.subtitle}>Documentos</h3>
+
+        
+
+    </div>;
 }
 
 export default DocumentosEstudiantePg;
