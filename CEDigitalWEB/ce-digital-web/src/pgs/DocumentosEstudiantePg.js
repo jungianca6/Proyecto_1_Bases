b2b8d0613331089aed0c2be69d2636ecd2a37627
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-05-17 21:48:17 -0600

    commit provisional

--- CEDigitalWEB/ce-digital-web/src/pgs/DocumentosEstudiantePg.js
+++ CEDigitalWEB/ce-digital-web/src/pgs/DocumentosEstudiantePg.js
@@ -3,13 +3,12 @@ import { Button, Card, Form } from 'react-bootstrap';
 import styles from './DocumentosEstudiantePg.module.css';
 
 function DocumentosEstudiantePg() {
-    return <div className={styles.documentosWrapper}>
+    return(<div className={styles.documentosWrapper}>
         <h1 className={styles.title}>CE Digital</h1>
         <h3 className={styles.subtitle}>Documentos</h3>
 
-        
-
-    </div>;
+    </div>
+    );
 }
 
 export default DocumentosEstudiantePg;
--- CEDigitalWEB/ce-digital-web/src/pgs/ProfesorPg.js
+++ CEDigitalWEB/ce-digital-web/src/pgs/ProfesorPg.js
@@ -3,9 +3,38 @@ import { useNavigate } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 import React from "react";
+import { Button, Card, Form } from 'react-bootstrap';
+import styles from './ProfesorPg.module.css';
 
 function ProfesorPg() {
-    return <div>Página del Profesor</div>;
+
+    const navigate = useNavigate();
+
+
+    return(
+        <div className={styles.profesorWrapper}>
+
+            <h1 className={styles.title}>CE Digital</h1>
+            <h3 className={styles.subtitle}>Profesor</h3>
+
+            {/* Botones de navegación */}
+            <Button
+                className={styles.cursosButton}>
+                Cursos
+            </Button>
+
+            <Button
+                className={styles.evaluacionesButton}>
+                Evaluaciones
+            </Button>
+
+            <Button
+                className={styles.documentosButton}>
+                Documentos
+            </Button>
+
+    </div>
+    );
   }
 
   export default ProfesorPg;
