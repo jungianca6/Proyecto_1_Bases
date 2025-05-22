7870ea6cd349898d37fdf1b8067db0e2cdabca15
Brayan Alpizar <brayanalpizar36@gmail.com>
2025-05-22 01:23:06 -0600

    AHORA SI

---
+++ CEDigital/Utilities/SQL_connection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CEDigital.Utilities
+{
+    public class SQL_connection
+    {
+        // Cadena de conexión a la base de datos
+        public string connection_string = "Server=localhost;Database=CEDigital;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        // Ejecuta la consulta y devuelve un SqlDataReader con los resultados
+        public SqlDataReader Execute_query(SqlCommand command, out SqlConnection connection)
+        {
+            connection = new SqlConnection(connection_string);
+            command.Connection = connection;
+            connection.Open();
+            return command.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public void Execute_non_query(SqlCommand command)
+        {
+            using (var connection = new SqlConnection(connection_string))
+            {
+                command.Connection = connection;
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+
+
+}
