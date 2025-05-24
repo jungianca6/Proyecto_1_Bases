37320bf2e95ce5d1e7db1e4e6ffe609be9168d8a
Kokigv <jorgutierrez@estudiantec.cr>
2025-05-23 23:16:36 -0600

    Arreglo de grading

--- CEDigital/Data_input_models/Data_input_add_default_grades.cs
+++ CEDigital/Data_input_models/Data_input_add_default_grades.cs
@@ -2,7 +2,7 @@
 {
     public class Data_input_add_default_grades
     {
-        public string course_code { get; set; }
+        public int group_id { get; set; }
         public List<string> sections { get; set; }
         public List<double> percentages { get; set; }
     }
